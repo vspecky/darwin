e1e94a41d69a2b63b12dcfd6e0d6812b2937f1cf
vspecky <thelonenerd9913@gmail.com>
2020-08-01 23:08:50 +0530

    lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub mod connection;
+pub mod genome;
+pub mod history;
+pub mod node;
+pub mod settings;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
