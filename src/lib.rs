1cab44b2c2b551b3e0f78aba90542e46998f126e
vspecky <thelonenerd9913@gmail.com>
2020-08-02 20:59:50 +0530

    Crate imports. Nothing to see here

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,6 @@ pub mod connection;
 pub mod genome;
 pub mod history;
 pub mod node;
+pub mod population;
 pub mod settings;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod species;
